2a724347064fa1c037306e49b6c91679b9446ba7
Sandeep Ghimire <s.ghimire@laserhub.de>
2018-07-05 00:03:51 +0200

    sandeep initial commit

---
+++ concat.js
@@ -0,0 +1,16 @@
+
+// loadsh javascript library scripts practise .
+const _ = require('lodash');
+
+var firstname = ['sandeep'];
+
+var name = _.concat(firstname, ['ghimire']);
+
+console.log(name);
+
+console.log(_.join(['sandeep', 'ghimire'], '-'));
+
+console.log(_.chunk(['a', 'b', 'c', 'd', 'e'], 2));
+
+
+console.log('sandeeo');
